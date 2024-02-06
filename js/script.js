600f5f2478a6bb43905816c6dd8527adfee556c3
Ferreira065 <157411146+Ferreira065@users.noreply.github.com>
2024-02-06 12:25:55 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function teste(){
+    let nome = document.getElementById("nome").value;
+    let senha = document.getElementById("senha").value;
+
+    if(nome != "juquinha" && senha !="123" ){
+        alert("Usuário ou senha inválidas")
+
+    }
+
+
+    console.log("Só alegria")
+    alert("Só alegria")
+    alert(nome);
+    alert(senha);
+}
